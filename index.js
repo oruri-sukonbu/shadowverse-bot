21af6bf80ee7beb87c285d1504a9821baeb3b43c
Haruto Morishima <harutomorishima@HarutonoMacBook-Air.local>
2026-03-23 16:12:45 +0900

    add express server

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const Parser = require('rss-parser');
 
